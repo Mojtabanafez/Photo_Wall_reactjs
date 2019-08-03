a64620debcaee007c7788a987f1f683c57a298d6
Mojtabanafez <nafezmojtan@gmail.com>
2019-08-03 02:37:02 -0400

    Design page front

---
+++ src/Components/Main.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import Title from './Title'
+import Photowall from './PhotoWall'
+
+
+// const posts = [{
+//     id: "0",
+//     description: "beautiful landscape",
+//     imageLink: " " + "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbehQmX7booN-lD3PhXReVX2aGdJNACig9m8GXWi9ZlucXlKuwIw"
+// }, {
+//     id: "1", description: "Aliens???",
+//         imageLink: " https://www.canva.com/learn/wp-content/uploads/2018/12/00-winterlandscapes_featimage.jpg" 
+// }, {
+//     id: "2",
+//     description: "On a vacation!",
+//         imageLink:"https://www.goodfreephotos.com/cache/italy/other-italy/beautiful-landscape-with-mountains-and-lakes-with-sky-in-italy_800.jpg?cached=1522446501"
+// }]
+
+class Main extends Component {
+    constructor(){
+        super();
+        this.state = {
+            posts:[{
+                id: "0",
+                description: "beautiful landscape",
+                imageLink: " " + "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbehQmX7booN-lD3PhXReVX2aGdJNACig9m8GXWi9ZlucXlKuwIw"
+            }, {
+                id: "1", description: "Aliens???",
+                imageLink: " https://www.canva.com/learn/wp-content/uploads/2018/12/00-winterlandscapes_featimage.jpg"
+            }, {
+                id: "2",
+                description: "On a vacation!",
+                imageLink: "https://www.goodfreephotos.com/cache/italy/other-italy/beautiful-landscape-with-mountains-and-lakes-with-sky-in-italy_800.jpg?cached=1522446501"
+            }]
+        }
+    }
+    // render() {
+    //     return <div>
+    //         <Title title={"Photowall"} />
+    //         <Photowall post={posts} />
+    //     </div>
+    // }
+    render() {
+        return <div>
+            <Title title={"Photowall"} />
+            <Photowall posts={this.state.posts}/>
+        </div>
+    }
+}
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import Main from './Components/Main'
+import './styles/stylesheet.css'
 
-const myfirstelement = <h1>Hello React!</h1>
 
-ReactDOM.render(myfirstelement, document.getElementById('root'));
+// const tasks = ['take out the trash', 'shovel the deiveway', 'walk the dog ']
+
+// const element1 = React.createElement('ol', null,
+//     React.createElement('li', null, 'Take out the trash'),
+//     React.createElement('li', null, 'Take out the trash'),
+//     React.createElement('li', null, ' walk the dog')
+// );
+
+// const element = React.createElement('ol', null,
+//     tasks.map((task, index) => React.createElement('li', {key:index}, task))
+// );
+
+
+//JSX
+// const element = <ol>
+//     <li>{tasks[0]}</li>
+//     <li>{tasks[1]}</li>
+//     <li>{tasks[2]}</li> 
+// </ol> 
+
+// const element =
+//     <div> 
+//         <h1> Task List </h1>
+//         <ol>
+//             {tasks.map((task, index)=><li key={index}>{task}</li>)}
+//         </ol>
+//     </div>
+
+// ReactDom.render(element, document.getElementById('root'));
+
+/*
+class List extends Component {
+    render() {
+        return (
+            <ol>
+                {this.props.tasks.map((task, index) => <li key={index}>{task}</li>)}
+            </ol>
+        )
+    }
+}
+
+class Title extends Component {
+    render() {
+        return <h1> {this.props.title} </h1>
+    }
+}
+class Main extends Component {
+    render() {
+        return <div>
+            <Title title={"Hello world"} />
+            <List tasks={['hose the driveway', 'finish the laundry']} />
+            <List tasks={['Mow the low', 'walk the dog']} />
+        </div>
+    }
+}
+*/
+
+ReactDom.render(<Main />, document.getElementById('root'));
